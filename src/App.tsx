a28840da5db0233fb3ef6ab5e01c0b087060cb6e
far_cds <sacofrerichs@gmail.com>
2026-03-17 13:50:08 -0400

    Implement correct structuring of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from '@lobehub/ui';
 import { BloodBrainBarrier } from './components/BloodBrainBarrier';
 
-function App() {
+export default function App() {
     return (
         <ThemeProvider themeMode="dark">
             <BloodBrainBarrier />
         </ThemeProvider>
     );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
-import { App } from './App';
+import App from './App';
 import './index.css';
 
 const rootEl = document.getElementById('root');
